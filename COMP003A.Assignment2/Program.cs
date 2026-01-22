01b48abc638212895d90dc74e1ca099ae1d7e0d0
samian <sam.iannazzo@yahoo.com>
2026-01-21 22:09:05 -0800

    added string to line 9 w/ readline / comment on line 7

--- COMP003A.Assignment2/Program.cs
+++ COMP003A.Assignment2/Program.cs
@@ -4,7 +4,11 @@
     {
         static void Main(string[] args)
         {
+            // writeline to display message asking for first name while the string with readline is waiting for user input
             Console.WriteLine("Please enter your first name");
+            string inputName = Console.ReadLine();
+
+
         }
     }
 }
